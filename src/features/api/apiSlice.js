e6fcfa6b95c500d57a1b38cb0bfa5f941a0a7028
mallyaagung <Mallyaagung@gmail.com>
2023-08-05 23:41:13 +0700

    change baseURL

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -2,7 +2,9 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:8000" }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://nutech-be-two.vercel.app",
+  }),
   tagTypes: ["Product"],
   endpoints: (builder) => ({}),
 });
